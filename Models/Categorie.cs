085639a8186eda2175150bb2e8fdaeaa94efcb70
Alexandra Sandu <sandu.alexandra99@yahoo.com>
2021-01-08 12:03:41 +0200

    Proiect

--- Models/Categorie.cs
+++ Models/Categorie.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;//folosit pentru constrangeri sau valori predefinite
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Sandu_Ana_Alexandra_Proiect.Models
 {
     public class Categorie
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
         public string NumeCategorie { get; set; }
         public ICollection<CategorieVacanta> CategoriiVacanta { get; set; }//relatie 1 to M cu CategorieVacanta
 
--- Models/Ghid.cs
+++ Models/Ghid.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;//folosit pentru constrangeri sau valori predefinite
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Sandu_Ana_Alexandra_Proiect.Models
 {
     public class Ghid
     {
         public int ID { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$", ErrorMessage = "Numele trebuie sa fie de forma 'Prenume Nume'"), StringLength(50, MinimumLength = 3)]
         public string NumeGhid { get; set; }
         public int Telefon { get; set; }
 
